59243ac56cab47672f49fb018ffa0f548ebe4daa
Rogalinski Michal <michal.rogalinski@tieto.com>
2018-12-30 18:15:31 +0100

    formatting somewhat working

--- src/formatprovider.ts
+++ src/formatprovider.ts
@@ -6,10 +6,19 @@ export default class XmlFormatProvider implements vscode.DocumentFormattingEditP
 	constructor(_context: vscode.ExtensionContext, _schemaPropertiesArray: Array<IXmlSchemaProperties>) {
 	}
 
-	provideDocumentFormattingEdits(textDocument: vscode.TextDocument, _options: vscode.FormattingOptions, _token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
+	provideDocumentFormattingEdits(textDocument: vscode.TextDocument, options: vscode.FormattingOptions, _token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
 		const format = require('xml-formatter');
-		let formattedText = format(textDocument.getText());
-		console.log(formattedText);
-        return [];
+		const indentationString = options.insertSpaces ? Array(options.tabSize).fill(" "): "\t";
+
+		let documentRange = new vscode.Range(textDocument.positionAt(0), textDocument.lineAt(textDocument.lineCount - 1).range.end);
+
+		const emptyLines = /^\s*[\r\n]/gm;
+		let formattedText  : string = format(textDocument.getText().trim(), {indentation: indentationString}).replace(emptyLines,"");
+
+		if (!formattedText) {
+			return [];
+		}
+
+        return [vscode.TextEdit.replace(documentRange, formattedText)];
 	}
 }
--- src/rangeformatprovider.ts
+++ src/rangeformatprovider.ts
@@ -6,10 +6,26 @@ export default class XmlRangeFormatProvider implements vscode.DocumentRangeForma
 	constructor(_context: vscode.ExtensionContext, _schemaPropertiesArray: Array<IXmlSchemaProperties>) {
 	}
 
-	provideDocumentRangeFormattingEdits(textDocument: vscode.TextDocument, _range: vscode.Range, _options: vscode.FormattingOptions, _token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
+	provideDocumentRangeFormattingEdits(textDocument: vscode.TextDocument, range: vscode.Range, options: vscode.FormattingOptions, _token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
 		const format = require('xml-formatter');
-		let formattedText = format(textDocument.getText());
-		console.log(formattedText);
-        return [];
+		const indentationString = options.insertSpaces ? Array(options.tabSize).fill(' ').join("") : "\t";
+
+		let before = textDocument.getText(new vscode.Range(textDocument.positionAt(0), range.start)).trim();
+
+		let selection = textDocument.getText(new vscode.Range(range.start, range.end)).trim();
+
+		let after = textDocument.getText(new vscode.Range(range.end, textDocument.lineAt(textDocument.lineCount - 1).range.end)).trim();
+
+		const selectionSeparator = "<!--352cf605-57c7-48a8-a5eb-2da215536443-->";
+		let text = [before, selection, after].join(selectionSeparator);
+
+		const emptyLines = /^\s*[\r\n]/gm;
+		let formattedText : string = format(text, {indentation: indentationString}).split(selectionSeparator)[1].replace(emptyLines,"");
+
+		if (!formattedText) {
+			return [];
+		}
+
+		return [vscode.TextEdit.replace(range, formattedText)];
 	}
 }
