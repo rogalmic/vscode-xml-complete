d46af11cd05fdc2e391182b0a09d4b9d2568ff62
ext-michal.rogalinski <michal.rogalinski@tieto.com>
2019-01-11 18:47:58 +0100

    some fixes

--- src/autocompletionprovider.ts
+++ src/autocompletionprovider.ts
@@ -6,110 +6,103 @@ import XmlSimpleParser from './helpers/xmlsimpleparser';
 export default class AutoCompletionProvider implements vscode.Disposable {
 
     private documentListener: vscode.Disposable;
-    private static maxTagChars = 100;
-    private delayCount: number = 0;
+    private static maxLineChars = 1024;
 
     constructor(protected extensionContext: vscode.ExtensionContext, protected schemaPropertiesArray: XmlSchemaPropertiesArray) {
         this.documentListener = vscode.workspace.onDidChangeTextDocument(async (evnt) =>
-            this.triggerDelayedAutoCompletion(evnt), this, this.extensionContext.subscriptions);
+            this.triggerAutoCompletion(evnt), this, this.extensionContext.subscriptions);
     }
 
     public dispose() {
         this.documentListener.dispose();
     }
 
-    private async triggerDelayedAutoCompletion(documentEvent: vscode.TextDocumentChangeEvent, timeout: number = 400): Promise<void> {
-        if (this.delayCount > 0) {
-            this.delayCount = timeout;
-            return;
-        }
-        this.delayCount = timeout;
-
-        const tick = 100;
-
-        while (this.delayCount > 0) {
-            await new Promise(resolve => setTimeout(resolve, tick));
-            this.delayCount -= tick;
-        }
-
-        this.triggerAutoCompletion(documentEvent);
-    }
-
     private async triggerAutoCompletion(documentEvent: vscode.TextDocumentChangeEvent): Promise<void> {
         const activeTextEditor = vscode.window.activeTextEditor;
         const document = documentEvent.document;
-
+        const inputChange = documentEvent.contentChanges[0];
         if (document.languageId !== languageId
             || documentEvent.contentChanges.length !== 1
-            || documentEvent.contentChanges[0].text.length > AutoCompletionProvider.maxTagChars
-            || !documentEvent.contentChanges[0].range.isSingleLine
-            || /\s/.test(documentEvent.contentChanges[0].text)
+            || !inputChange.range.isSingleLine
             || activeTextEditor === undefined
             || activeTextEditor.document.uri.toString() !== document.uri.toString()) {
             return;
         }
 
-        const inputChange = documentEvent.contentChanges[0];
-        let inputChangePosition = inputChange.range.end;
 
-        for (let i = 0; i < AutoCompletionProvider.maxTagChars
-            && document.getText(new vscode.Range(inputChangePosition, inputChangePosition.translate(0, 1))) !== ">"; i++) {
+        const linePosition = inputChange.range.end.character - 1;
+        const changeLine = inputChange.range.end.line;
+        const wholeLineRange = document.lineAt(changeLine).range;
+        const wholeLineText = document.getText(document.lineAt(inputChange.range.end.line).range);
 
-            inputChangePosition = inputChangePosition.translate(0, 1);
+        if (wholeLineText.length >= AutoCompletionProvider.maxLineChars) {
+            return;
         }
 
-        let inputChangeEndPosition = inputChangePosition.translate(0, 1);
+        const before = wholeLineText.substring(0, linePosition);
+        const after = wholeLineText.substring(linePosition);
 
-        for (let i = 0; i < AutoCompletionProvider.maxTagChars
-            && document.getText(new vscode.Range(inputChangeEndPosition, inputChangeEndPosition.translate(0, 1))) !== ">"; i++) {
+        let scope = await XmlSimpleParser.getScopeForPosition(wholeLineText, linePosition);
 
-            inputChangeEndPosition = inputChangeEndPosition.translate(0, 1);
+        if (!(scope.context && scope.context !== "text" && scope.tagName)) {
+            console.log(`Unknown scope for char ${linePosition} in '${wholeLineText}'`);
+            return;
+        }
+
+        if (before.substr(before.lastIndexOf("<"), 2) === "</") {
+            // NOTE: current position in closing tag
+            return;
+        }
+
+        // NOTE: auto-change is available only for single tag enclosed in one line
+        const closeCurrentTagIndex = after.indexOf(">");
+        const nextTagStartPostion = after.indexOf("<");
+        const nextTagEndingPostion = nextTagStartPostion >= 0 ? after.indexOf(">", nextTagStartPostion) : -1;
+        const invalidTagStartPostion = nextTagEndingPostion >= 0 ? after.indexOf("<", nextTagEndingPostion) : -1;
+
+        let resultText: string = "";
+
+        if (after.substr(closeCurrentTagIndex - 1).startsWith(`/></${scope.tagName}>`) && closeCurrentTagIndex === 2) {
+
+            resultText = wholeLineText.substring(0, linePosition + nextTagStartPostion) + `` + wholeLineText.substring(linePosition + nextTagEndingPostion + 1);
+
+        } else if (after.substr(closeCurrentTagIndex - 1, 2) !== "/>" && invalidTagStartPostion < 0) {
+
+            if (nextTagStartPostion >= 0 && after[nextTagStartPostion + 1] === "/") {
+
+                resultText = wholeLineText.substring(0, linePosition + nextTagStartPostion) + `</${scope.tagName}>` + wholeLineText.substring(linePosition + nextTagEndingPostion + 1);
+            }
+            else if (nextTagStartPostion < 0) {
+                resultText = wholeLineText.substring(0, linePosition + closeCurrentTagIndex + 1) + `</${scope.tagName}>` + wholeLineText.substring(linePosition + closeCurrentTagIndex + 1);
+            }
+        }
+
+        if (!resultText) {
+            return;
+        }
+
+        resultText = resultText.trimRight();
+
+        if (!await XmlSimpleParser.checkXml(`<root>${resultText}</root>`)) {
+            return;
         }
 
         let documentContent = document.getText();
-        let offset = document.offsetAt(inputChangePosition);
 
-        let scope = await XmlSimpleParser.getScopeForPosition(documentContent, offset - 1);
+        documentContent = documentContent.split("\n")
+            .map((l, i) => (i === changeLine) ? resultText : l)
+            .join("\n");
 
-        if (scope.context && scope.tagName && documentContent[offset - 1] !== "/" && scope.content.indexOf("</") < 0) {
-
-            const before = document.getText(new vscode.Range(document.positionAt(0), inputChangePosition.translate(0, 1)));
-            const insertion = `</${scope.tagName}>`;
-            const after = document.getText(new vscode.Range(inputChangeEndPosition.translate(0, 1), document.lineAt(document.lineCount - 1).range.end));
-
-            documentContent = before + insertion + after;
-
-            if (!await XmlSimpleParser.checkXml(documentContent)) {
-                return;
-            }
-
-            await activeTextEditor.edit((builder) => {
-                builder.replace(
-                    new vscode.Range(
-                        inputChangePosition.translate(0, 1),
-                        inputChangeEndPosition.translate(0, 1)),
-                    insertion);
-            }, { undoStopAfter: false, undoStopBefore: false });
+        if (!await XmlSimpleParser.checkXml(documentContent)) {
+            return;
         }
-        else if (scope.context && scope.tagName && documentContent[offset - 1] === "/") {
 
-            const before = document.getText(new vscode.Range(document.positionAt(0), inputChangePosition));
-            const insertion = `>`;
-            const after = document.getText(new vscode.Range(inputChangeEndPosition.translate(0, 1), document.lineAt(document.lineCount - 1).range.end));
-
-            documentContent = before + insertion + after;
-
-            if (!await XmlSimpleParser.checkXml(documentContent)) {
-                return;
-            }
-
-            await activeTextEditor.edit((builder) => {
-                builder.replace(
-                    new vscode.Range(
-                        inputChangePosition,
-                        inputChangeEndPosition.translate(0, 1)),
-                    insertion);
-            }, { undoStopAfter: false, undoStopBefore: false });
-        }
+        await activeTextEditor.edit((builder) => {
+            builder.replace(
+                new vscode.Range(
+                    wholeLineRange.start,
+                    wholeLineRange.end),
+                resultText);
+        }, { undoStopAfter: false, undoStopBefore: false });
     }
 }
